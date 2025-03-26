bec7937970574d3e0ac0fc76279b0ba581a14222
Ravi kumar <rk5118754@gmail.com>
2025-03-26 15:27:57 +0530

    Updated vite.config.ts and main.tsx for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: "/MyPortfolio-Ravi/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
